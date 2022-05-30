bc86675d06f6b974a14f6a5d559bdaad94993da7
longle <hjjddbz@gmail.com>
2022-05-31 02:48:01 +0700

    Fix missing last node

--- algorithms/ant_colony_optimize.h
+++ algorithms/ant_colony_optimize.h
@@ -3,45 +3,51 @@
 
 using std::cout;
 
+class Result
+{
+public:
+  double total_distance;
+  vector<Path> paths;
 
-class Result {
-  public:
-    double total_distance;
-    vector<Path> paths;
+  Result()
+  {
+    this->total_distance = LLONG_MAX;
+    paths.clear();
+  }
 
-    Result(){
-      this->total_distance = LLONG_MAX;
-      paths.clear();
+  Result(vector<Path> paths)
+  {
+    this->total_distance = 0;
+    for (Path &path : paths)
+      this->total_distance += path.total_distance;
+    this->paths = paths;
+  }
+
+  std::string to_string()
+  {
+    std::string res = "routers:\n";
+    for (Path &path : paths)
+    {
+      res += "\t" + path.to_string();
     }
+    res += "total_distance " + std::to_string(total_distance);
+    return res;
+  }
 
-    Result(vector<Path> paths){
-      this->total_distance = 0;
-      for (Path &path : paths)
-        this->total_distance += path.total_distance;
-      this->paths = paths;
-    }
-
-    std::string to_string() {
-      std::string res = "routers:\n";
-      for (Path &path: paths){
-        res += "\t" + path.to_string();
-      }
-      res += "total_distance " + std::to_string(total_distance);
-      return res;
-    }
-
-    bool operator < (const Result &other){
-      return this->total_distance <= other.total_distance;
-    }
+  bool operator<(const Result &other)
+  {
+    return this->total_distance <= other.total_distance;
+  }
 };
 
-std::ostream &operator << (std::ostream &cout, Result &result){
+std::ostream &operator<<(std::ostream &cout, Result &result)
+{
   cout << result.to_string();
   return cout;
-} 
+}
 
-
-Result aco(BaseGraph baseGraph, int num_ants, double alpha, double beta, double gamma, double ro, int max_trial){
+Result aco(BaseGraph baseGraph, int num_ants, double alpha, double beta, double gamma, double ro, int max_trial)
+{
   int MAX_NOT_BETTER_TRY = max_trial;
 
   ACOGraph graph(baseGraph);
@@ -49,69 +55,80 @@ Result aco(BaseGraph baseGraph, int num_ants, double alpha, double beta, double 
   Result result;
   int not_better_try(0);
   int num_trial(0);
-  cout << "Trial cofiguration:" << 
-          "\n\t num_vehicile(s): " << num_ants <<
-          "\n\t alpha: " << alpha << 
-          "\n\t beta: " << beta <<
-          "\n\t gamma: " << gamma << 
-          "\n\t ro: " << ro << '\n';
-  while(not_better_try < MAX_NOT_BETTER_TRY){
+  cout << "Trial cofiguration:"
+       << "\n\t num_vehicile(s): " << num_ants << "\n\t alpha: " << alpha << "\n\t beta: " << beta << "\n\t gamma: " << gamma << "\n\t ro: " << ro << '\n';
+  while (not_better_try < MAX_NOT_BETTER_TRY)
+  {
     // cout << "Trial " << num_trial++ << '\n';
     srand(time(NULL));
-    
+
     vector<Path> paths;
     vector<bool> visited(graph.num_nodes, false);
     visited[graph.start_node.id] = true;
-    int num_not_visited = graph.num_nodes-1;
+    int num_not_visited = graph.num_nodes - 1;
 
-    for (int k=0; k<num_ants && num_not_visited>0; ++k){
+    // create route for each ant
+    for (int k = 0; k < num_ants && num_not_visited > 0; ++k)
+    {
       Path path;
       path.add_node(graph.start_node);
       double capability = graph.max_capability;
       Node current_node = graph.start_node;
 
-      while(num_not_visited>0){
-        vector<double> prob(graph.num_nodes); // prob[i] is probability of ant[k] go from current node to node[i]
+      while (num_not_visited > 0)
+      {
+        vector<double> prob(graph.num_nodes);        // prob[i] is probability of ant[k] go from current node to node[i]
         vector<std::pair<double, int>> cucumProb(1); // cucumlative the probabilities to generate a random choice of ant[k]
 
-        for(int i=0; i<graph.num_nodes; ++i){
-          if (!visited[i] && capability>=graph.nodes[i].quantity){
+        for (int i = 0; i < graph.num_nodes; ++i)
+        {
+          // visitable nodes
+          if (!visited[i] && capability >= graph.nodes[i].quantity)
+          {
             prob[i] = pow(graph.pheromone[current_node.id][i], alpha) + pow(graph.heuristic[current_node.id][i], beta);
-            cucumProb.push_back(std::make_pair(cucumProb.back().first+prob[i], i));
+            cucumProb.push_back(std::make_pair(cucumProb.back().first + prob[i], i));
           }
         }
-        if (cucumProb.size()==1){
+        // no visitable nodes found
+        if (cucumProb.size() == 1)
+        {
           path.add_node(graph.start_node);
           break;
         }
+        // simulate the random choice between visitable nodes
         int chosen_id = (*std::upper_bound(cucumProb.begin(), cucumProb.end(), std::make_pair(randf() * cucumProb.back().first, -1))).second;
         path.add_node(graph.nodes[chosen_id]);
         capability -= graph.nodes[chosen_id].quantity;
         visited[chosen_id] = true;
         --num_not_visited;
         current_node = graph.nodes[chosen_id];
+        if (num_not_visited == 0)
+          path.add_node(graph.start_node);
       }
-
       paths.push_back(path);
     }
     // for (Path &path: paths)
     //   std::cout << path << '\n';
+
     // Create delta matrix
     Matrix delta(graph.num_nodes, graph.num_nodes);
-    for (Path &path : paths){
-      for(int i=0; i<path.trace.size()-1; ++i){
-        delta[i][i+1] += double(1)/path.total_distance;
-        delta[i+1][i] += double(1)/path.total_distance;
+    for (Path &path : paths)
+    {
+      for (int i = 0; i < path.trace.size() - 1; ++i)
+      {
+        delta[i][i + 1] += double(1) / path.total_distance;
+        delta[i + 1][i] += double(1) / path.total_distance;
       }
     }
     // Update pheromone matrix
-    graph.pheromone *= (1-ro);
+    graph.pheromone *= (1 - ro);
     graph.pheromone += delta;
 
     // Ignore if the route is not finished
     // Create result for the trial
     Result current_result(paths);
-    if (result < current_result || num_not_visited){
+    if (result < current_result || num_not_visited)
+    {
       ++not_better_try;
       continue;
     }
@@ -121,4 +138,3 @@ Result aco(BaseGraph baseGraph, int num_ants, double alpha, double beta, double 
   }
   return result;
 }
-
--- main.cpp
+++ main.cpp
@@ -11,19 +11,19 @@
 #include "algorithms/ant_colony_optimize.h"
 
 // using namespace std;
-using std::ifstream;
-using std::vector;
 using std::cout;
+using std::ifstream;
 using std::string;
+using std::vector;
 
 // template<class T> using orderedSet = tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-
 
 // vector<int> findBestMatch(BiGraph biGraph, string option){
 //   if (option == "HUNGARIAN") return HungarianSolver(biGraph).solution;
 // }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
   string output = string(argv[1]) + ".out";
   freopen(output.c_str(), "w", stdout);
   BaseGraph graph = read_input(argv[1]);
@@ -32,21 +32,25 @@ int main(int argc, char** argv){
   // cout << graph.toString();
   Result final_result;
   int num_tried = 0;
-  for (int num_ants=graph.num_nodes/12; num_ants<=graph.num_nodes&&num_tried<5; ++num_ants){
+  for (int num_ants = graph.num_nodes / 12; num_ants <= graph.num_nodes && num_tried < 500; ++num_ants)
+  {
     double alpha(2), beta(5), gamma(9), ro(0.8);
-    // for (double alpha=0.2; alpha<=2; alpha+=0.2)
-    //   for (double beta=0.2; beta<=2; beta+=0.2)
-    //     for (double gamma=0.2; gamma<=2; gamma+=0.2)
-    //       for (double ro=0; ro<=1; ro+=0.2){
+    // for (double alpha = 0; alpha <= 20; alpha += 0.2)
+    //   for (double beta = 0; beta <= 20; beta += 0.2)
+    //     for (double gamma = 0; gamma <= 20; gamma += 0.2)
+    //       for (double ro = 0; ro <= 1; ro += 0.01)
+    {
       Result current_result = aco(graph, num_ants, alpha, beta, gamma, ro, 10);
-      if (current_result < final_result){
+      if (current_result < final_result)
+      {
         final_result = current_result;
         num_tried = 0;
       }
-    // }
+    }
     ++num_tried;
     // cout << "result with " + std::to_string(num_ants) + " vehicle(s):\n";
     //   cout << final_result << '\n';
   }
-  cout << "\nFinal result: \n" << final_result << "\nDone!\n";
+  cout << "\nFinal result: \n"
+       << final_result << "\nDone!\n";
 }
